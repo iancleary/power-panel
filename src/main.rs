17d1647207fb3ea39c344664257520a0420e4f8d
iancleary <github@iancleary.me>
2023-09-17 15:02:19 -0700

    Initial template

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use iced::Settings;
+use iced::pure::widget::{Button, Column, Container, Text};
+use iced::pure::Sandbox;
+
+fn main() -> Result<(), iced::Error> {
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    count: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum CounterMessage {
+    Increment,
+    Decrement,
+}
+
+impl Sandbox for Counter {
+    type Message = CounterMessage;
+
+    fn new() -> Self {
+        Counter { count: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter app")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            CounterMessage::Increment => self.count += 1,
+            CounterMessage::Decrement => self.count -= 1,
+        }
+    }
+
+    fn view(&self) -> iced::pure::Element<Self::Message> {
+        let label = Text::new(format!("Count: {}", self.count));
+        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
+        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
+        let col = Column::new().push(incr).push(label).push(decr);
+        Container::new(col).center_x().center_y().width(iced::Length::Fill).height(iced::Length::Fill).into()
+    }
+}
