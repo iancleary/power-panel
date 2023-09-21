5f5fa264d87fc8cc507b9026ba5628e2f8ae95e8
iancleary <github@iancleary.me>
2023-09-21 12:52:12 -0700

    Attempt to solve pkg-config nix build issues, WIP

--- default.nix
+++ default.nix
@@ -7,12 +7,18 @@ pkgs.rustPlatform.buildRustPackage rec {
   version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
+
   nativeBuildInputs = with pkgs; [
-      pkg-config
-      gtk4
-    ];
-  buildInputs = [
+    pkg-config
   ];
+
+  buildInputs = with pkgs; [
+    wayland
+    gtk4
+  ];
+
+  # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+
 }
 
 
--- shell.nix
+++ shell.nix
@@ -6,7 +6,9 @@ pkgs.mkShell {
   # Additional tooling
   buildInputs = with pkgs; [
     rustup       # Rust toolchain
+    pkg-config   # Build tool
     just         # Task runner
+    gtk4         # GUI toolkit
   ];
 
   # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
